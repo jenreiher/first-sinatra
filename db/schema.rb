7aa964aa3bb75f94dbd6d4db9a7e2dfcbae49383
Jen <jen@reiher.ca>
2016-06-10 21:20:18 +0000

    completed steps 1-3

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160610211353) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "content"
+  end
 
 end
